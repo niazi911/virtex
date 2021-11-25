18d715e4d65d754ae15e08bda81e046db0d42973
niazi911 <88917317+niazi911@users.noreply.github.com>
2021-11-25 07:17:56 +0500

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,256 @@
+#GIVE_COPY_Right
+#p = "\x1b[0;37m" # 
+"""
+q="\033[00m"
+h2="\033[40m"
+b2="\033[44m"
+c2="\033[46m"
+i2="\033[42m"
+u2="\033[45m"
+m2="\033[41m"
+p2="\033[47m"
+k2="\033[43m"
+b='\033[1;94m'
+i='\033[0;92m'
+c='\033[0;96m'
+m='\033[0;91m'
+u='\033[0;95m'
+k='\033[0;93m'
+p='\033[0;97m'
+h='\033[0;90m'
+p = "\x1b[0;33m" # 
+m = "\x1b[0;31m" # 
+h = "\x1b[0;32m" # 
+k = "\033[0;36m" # 
+b = "\x1b[0;34m" #
+u = "\x1b[0;35m" # 
+o = "\033[0;32m" # 
+""" # WARNA ASLI
+q="\033[00m"
+h2="\033[40m"
+b2="\033[44m"
+c2="\033[46m"
+i2="\033[42m"
+u2="\033[45m"
+m2="\033[41m"
+p2="\033[47m"
+k2="\033[43m"
+b='\033[0;94m'
+B='\033[0;94m'
+I='\033[0;92m'
+C='\033[0;96m'
+M='\033[0;91m'
+U='\033[0;95m'
+K='\033[0;93m'
+P='\033[0;97m'
+H='\033[0;90m'
+i='\033[0;92m'
+c='\033[0;92m'
+m='\033[0;91m'
+u='\033[0;95m'
+k='\033[0;96m'
+p='\033[0;93m'
+h='\033[0;90m'
+
+m3=(q+m2)
+p = "\x1b[0;33m" # White
+m = "\x1b[0;31m" # Red
+h = "\x1b[0;32m" # green
+#k = "\033[0;36m" # yellow
+b = "\x1b[0;34m" # blue
+u = "\x1b[0;35m" # purple
+o = "\033[0;32m" # light blue
+# This Color Is Broken
+bulat=(k+"["+p+"•"+k+"] "+p)   #   [•]
+war=(k+"["+p+"!"+k+"] "+p)  # [!]
+inp=(k+"["+p+"?"+k+"] "+p) # [?]
+bulat2=(k+"["+p+"••"+k+"] "+p)   # [••]
+war2=(k+"["+p+"!!"+k+"] "+p)  # [!!]
+inp2=(k+"["+p+"??"+k+"] "+p) # [??]
+bulat=(k+"["+p+"•"+k+"] "+p)   #   [•]
+war=(k+"["+p+"!"+k+"] "+p)  # [!]
+inp=(k+"["+p+"?"+k+"] "+p) # [?]
+bulat2=(k+"["+p+"••"+k+"] "+p)   # [••]
+war2=(k+"["+p+"!!"+k+"] "+p)  # [!!]
+inp2=(k+"["+p+"??"+k+"] "+p) # [??]
+garis=(k+'['+i+'+'+k+']'+p+'========================================================='+k+'['+i+'+'+k+']')
+import os
+import sys
+import time
+import datetime
+import random
+import hashlib
+import re
+import threading
+import json
+import urllib
+try: import requests
+except ModuleNotFoundError: os.system("python -m pip install requests &> /dev/null")
+try: import bs4
+except ModuleNotFoundError: os.system("python -m pip install bs4 &> /dev/null")
+try: import mechanize
+except ModuleNotFoundError: os.system("python -m pip install mechanize &> /dev/null")
+import requests as req
+try:
+        import requests
+except ImportError:
+        print ('[×] Module installing...\n')
+        os.system('pip install requests' if os.name == 'nt' else 'pip2 install requests')
+import requests
+import uuid
+import ipaddress
+import calendar
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup as par
+from time import sleep
+from datetime import datetime
+from datetime import date
+import requests,mechanize,bs4,sys,os,subprocess,uuid
+import requests,sys,random,time,re,base64,json
+import os, re, requests, concurrent.futures
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from datetime import date
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup as parser
+import requests as r, re, os
+from bs4 import BeautifulSoup as par
+import platform
+import requests, bs4, sys, os, subprocess, random, time, re, json
+import concurrent.futures
+from datetime import datetime
+from time import sleep
+from requests import Session
+import re, sys
+import sys
+from os import system
+import os, sys, time, random
+from sys import exit as keluar
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from sys import stdout
+from os import system
+import re
+import os,random,time,sys
+import json
+from time import sleep as waktu
+from bs4 import BeautifulSoup as parser
+current = datetime.now()
+import requests,mechanize,bs4,sys,os,subprocess,uuid,random,time,re,base64,concurrent.futures,json
+koneksi_error=(req.exceptions.ConnectionError,req.exceptions.ChunkedEncodingError,req.exceptions.ReadTimeout)
+
+####MUHAMMAD/BILAL/TECH-QAISER
+import requests,bs4,sys,os,random,time,re,json,uuid,subprocess
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from bs4 import BeautifulSoup as par
+from datetime import date
+from datetime import datetime
+from urllib.parse import quote
+
+#####ABDULLAH___DADA
+import requests, sys, bs4, os, random, time, re, json
+from concurrent.futures import ThreadPoolExecutor as zthreads
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup as parser
+from datetime import datetime
+from time import sleep
+
+####UA
+ua = {"user-agent":"chrome"}
+prvt = []
+ses = r.Session()
+link = "https://free.facebook.com/"
+r=requests.Session()
+#sdj_prvt
+N="\033[00m"
+q="\033[00m"
+h2="\033[40m"
+b2="\033[44m"
+c2="\033[46m"
+i2="\033[42m"
+u2="\033[45m"
+m2="\033[41m"
+p2="\033[47m"
+k2="\033[43m"
+b='\033[0;34m'
+i='\033[0;32m'
+c='\033[0;36m'
+m='\033[0;31m'
+u='\033[0;35m'
+k='\033[0;33m'
+p='\033[0;37m'
+h='\033[0;90m'
+b='\033[0;34m'
+I='\033[0;32m'
+C='\033[0;36m'
+M='\033[0;31m'
+U='\033[0;35m'
+K='\033[0;33m'
+P='\033[0;37m'
+H='\033[0;90m'
+Q="\033[00m"
+warna_me=([i, c, m, u, k, p, h, b])
+warna_ms=([i, c, m, u, k, p, h, b])
+w2=(warna_me)
+w=random.choice(w2)
+try:
+	z = 50
+	x = 0
+except (KeyError, IOError):
+	exit(war+"BUG !!")
+try:
+	kntlx=([i, c, m, u, k, p, h, b])
+	for w3 in kntlx:
+		continue
+except KeyError:
+	w3 = ("")
+host="https://mbasic.facebook.com"
+ips=None
+try:
+        ipx=requests.get("http://ip-api.com/json/").json()["query"]
+        ips=requests.get("http://ip-api.com/json/"+ipx,headers={"Referer":"http://ip-api.com/","Content-Type":"application/json; charset=utf-8","User-Agent":"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"}).json()["country"].lower()
+        ipp = requests.get("https://api.ipify.org").text
+        country=requests.get("http://ip-api.com/json/").json()["country"]
+except:
+        ips=None
+        ipx=("NONE")
+
+
+
+def kntl():
+        jjja = ([Q, C, I, B, U, M, K, H])
+        warn = (jjja)
+#        random.choice(sdj)
+#             "< L O A D I N G >"
+        _1_ = ("%s<"%(C))
+        _2_ = ("%s< "%(C))
+        _3_ = ("%s< %sL"%(C,random.choice(warn)))
+        _4_ = ("%s< %sL "%(C,random.choice(warn)))
+        _5_ = ("%s< %sL %sO"%(C,random.choice(warn),random.choice(warn)))
+        _6_ = ("%s< %sL %sO "%(C,random.choice(warn),random.choice(warn)))
+        _7_ = ("%s< %sL %sO %sA"%(C,random.choice(warn),random.choice(warn),random.choice(warn)))
+        _8_ = ("%s< %sL %sO %sA "%(C,random.choice(warn),random.choice(warn),random.choice(warn)))
+        _9_ = ("%s< %sL %sO %sA %sD"%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _10_ =("%s< %sL %sO %sA %sD "%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _11_ =("%s< %sL %sO %sA %sD %sI"%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _12_ =("%s< %sL %sO %sA %sD %sI "%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _13_ =("%s< %sL %sO %sA %sD %sI %sN"%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _14_ =("%s< %sL %sO %sA %sD %sI %sN "%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _15_ =("%s< %sL %sO %sA %sD %sI %sN %sG"%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _16_ =("%s< %sL %sO %sA %sD %sI %sN %sG "%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn)))
+        _17_ =("%s< %sL %sO %sA %sD %sI %sN %sG %s>"%(C,random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),C))
+        
+        _99_ = ([_1_, _2_, _3_, _4_, _5_, _6_, _7_,  _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, _17_, _17_,  _16_,  _15_,  _14_,  _13_,  _12_,  _11_,  _10_,  _9_,  _8_,  _7_,  _6_,  _5_, _4_,  _3_,  _2_,  _1_])
+        for x in _99_:
+            stdout.write("\r %s[%s+%s]%s=%s=%s=%s=%s⟩%s⟩ %s"%(random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),random.choice(warn),x))
+            stdout.flush(),;time.sleep(0.30)
+def logo_mt():
+#### @SDJ
+	
+	print(dmi_2)
+####   @RECODER___LOVE__U😚😀😃😃😂
